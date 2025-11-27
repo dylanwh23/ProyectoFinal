1e1bc40decae3391ef5a59cc6879259130e69d8b
Supraim <supraimofc@gmail.com>
2025-11-27 05:33:53 -0300

    Data

---
+++ src/EventProcessor.Worker/Data/EventDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Contracts.Models;
+
+namespace EventProcessor.Worker.Data;
+
+public class EventDbContext : DbContext
+{
+    public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) { }
+
+    public DbSet<EnrichedEvent> Events { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EnrichedEvent>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.EventId).IsUnique();
+            entity.HasIndex(e => e.IpCamara);
+            entity.HasIndex(e => e.MomentoOriginal);
+            entity.HasIndex(e => e.ProcesadoEn);
+
+            entity.Property(e => e.MensajeCrudoEvento)
+                  .HasColumnType("TEXT");
+
+            entity.Property(e => e.VideoLink)
+                  .HasMaxLength(500);
+        });
+    }
+}
